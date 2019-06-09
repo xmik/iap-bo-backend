4958d5b844f492f3f69f2ae14e8fac3888b6a7de
Ewa Czechowska <ewa@ai-traders.com>
2019-06-09 21:10:40 +0200

    set different defaults for employeeHours

--- src/BranchOfficeBackend/DBModel/EmployeeHours.cs
+++ src/BranchOfficeBackend/DBModel/EmployeeHours.cs
@@ -19,6 +19,33 @@ namespace BranchOfficeBackend
 
         public int EmployeeId { get; set; }
 
+        public EmployeeHours()
+        {
+            EmployeeHoursId = -1;
+            EmployeeId = -1;
+            Value = -1;
+            HoursCount = -1;
+        }
+
+        public EmployeeHours(WebEmployeeHours webEmployeeHours)
+        {
+            this.HoursCount = webEmployeeHours.HoursCount;
+            this.Value = webEmployeeHours.Value;
+            this.TimePeriod = webEmployeeHours.TimePeriod;
+            this.EmployeeId = webEmployeeHours.EmployeeId;
+            // this will be set by db
+            this.EmployeeHoursId = -1;
+        }
+
+        public EmployeeHours(EmployeeHours employeeHours, int v)
+        {
+            this.HoursCount = employeeHours.HoursCount;
+            this.Value = employeeHours.Value;
+            this.TimePeriod = employeeHours.TimePeriod;
+            this.EmployeeId = employeeHours.EmployeeId;
+            this.EmployeeHoursId = v;
+        }
+
         public override string ToString()
         {
             return String.Format("Id: {0}, Value: {1}, TimePeriod: {2}, EmployeeId {3}, HoursCount {4}",
